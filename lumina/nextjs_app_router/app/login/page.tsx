cd7e2c60cef219fe55193f8a08a1c693a796e522
zainrana558 <zainrana553@gmail.com>
2026-05-21 02:08:26 -0700

    Updated page.tsx

--- lumina/nextjs_app_router/app/login/page.tsx
+++ lumina/nextjs_app_router/app/login/page.tsx
@@ -1,6 +1,8 @@
 
 import LoginCard from '../../components/Auth/LoginCard';
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
     return (
         <div className="min-h-screen bg-black flex items-center justify-center">
